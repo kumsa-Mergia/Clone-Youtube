b17f9402eefc2adfc75c2bcf420a4851b19d8ce6
kumsa Mergia <kumsamergia@gmail.com>
2025-03-01 11:24:41 +0300

    Sidbar Updated

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -18,109 +18,148 @@ import { GiTrophyCup } from "react-icons/gi";
 import { BsLightbulb } from "react-icons/bs";
 import { FaBasketShopping } from "react-icons/fa6";
 import { RiArrowDropDownLine } from "react-icons/ri";
+import { RiDownloadLine } from "react-icons/ri";
 
 import Image1 from "../../assets/img/Kana.jpg";
 import Image2 from "../../assets/img/MrBeast.jpg";
 
-const Sidebar = ({sidebar}) => {
+const Sidebar = ({ sidebar }) => {
   return (
     <>
-      <div className="p-5 bg-gray-100 font-sans text-1xl w-54 min-h-screen bg-white"> 
-      
+      {/* <div className={`p-5 bg-gray-100 font-sans bg-white ${sidebar ? "text-1xl w-54" : "w-24"}`}> */}
+      <div
+        className={`bg-gray-100 font-sans bg-white ${
+          sidebar ? 'text-1xl w-54 p-5 ' : 'w-24'
+        }`}
+      >
         {/* First  */}
         <div>
-          <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
-            <MdHome className="h-5 w-5 mr-3"/>
-            <p  className={` ${sidebar?"":"hidden"}`}>Home</p>
+        <div
+            className={`flex mb-2 p-2 rounded hover:bg-gray-200 ${
+              sidebar ? '' : 'grid p-1'
+            } items-center`}
+          >
+            <p className={` ${sidebar ? 'h-5 w-5' : 'h-8 w-8'}`}>
+              <MdHome className={`${sidebar ? '' : 'h-full w-full m-4'}`} />
+            </p>
+            <p className={` ${sidebar ? '' : 'p-3 '}`}>Home</p>
           </div>
-          <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
-            <SiYoutubeshorts className="h-5 w-5 mr-3" />
-            <p  className={` ${sidebar?"":"hidden"}`}>Shorts</p>
+
+          <div
+            className={`flex mb-2 p-2 rounded hover:bg-gray-200 ${
+              sidebar ? '' : 'grid'
+            } items-center`}
+          >
+            <p className={` ${sidebar ? 'h-5 w-5' : 'h-8 w-8'}`}>
+              <SiYoutubeshorts
+                className={`${sidebar ? '' : 'h-full w-full m-4'}`}
+              />
+            </p>
+            <p className={` ${sidebar ? '' : 'p-3  '}`}>Shorts</p>
           </div>
-          <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
-            <MdOutlineSubscriptions className="h-5 w-5 mr-3" />
-            <p  className={` ${sidebar?"":"hidden"}`}>Subscriptions</p>
+
+          <div
+            className={`flex mb-2 p-2 rounded hover:bg-gray-200 ${
+              sidebar ? '' : 'grid mb-1'
+            } items-center`}
+          >
+            <p className={` ${sidebar ? 'h-5 w-5' : 'h-8 w-8'}`}>
+              <MdOutlineSubscriptions
+                className={`${sidebar ? '' : 'h-full w-full m-4'}`}
+              />
+            </p>
+            <p className={` ${sidebar ? '' : 'p-3 '}`}>Subscriptions</p>
           </div>
-          <hr className="w-10"/>
+
+          <div
+            className={`flex mb-2 p-2 rounded hover:bg-gray-200 ${
+              sidebar ? '' : 'grid'
+            } items-center`}
+          >
+            <p className={` ${sidebar ? 'h-5 w-5' : 'h-8 w-8'}`}>     
+              <RiDownloadLine className={`${sidebar ? '' : 'h-full w-full m-4'}`} />
+            </p>
+            <p className={` ${sidebar ? '' : 'p-3 '}`}>Downloads</p>
+          </div>
+          <hr className="w-10" />
         </div>
         {/* You */}
-        <div>
-          <h3 className="font-bold">You  </h3>
+        <div className={` ${sidebar ? "" : "hidden"}`}>
+          <h3 className="font-bold">You </h3>
           <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
             <FaHistory className="h-5 w-5 mr-3" />
-            <p  className={` ${sidebar?"":"hidden"}`}>History</p>
+            <p className={` ${sidebar ? "" : "hidden"}`}>History</p>
           </div>
           <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
             <MdOutlinePlaylistPlay className="h-5 w-5 mr-3" />
-            <p  className={` ${sidebar?"":"hidden"}`}>Playlists</p>
+            <p className={` ${sidebar ? "" : "hidden"}`}>Playlists</p>
           </div>
           <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
             <CiYoutube className="h-5 w-5 mr-3" />
-            <p  className={` ${sidebar?"":"hidden"}`}>Your Videos</p>
+            <p className={` ${sidebar ? "" : "hidden"}`}>Your Videos</p>
           </div>
           <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
             <MdOutlineWatchLater className="h-5 w-5 mr-3" />
-            <p  className={` ${sidebar?"":"hidden"}`}>Watch later</p>
+            <p className={` ${sidebar ? "" : "hidden"}`}>Watch later</p>
           </div>
           <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
             <BiLike className="h-5 w-5 mr-3" />
-            <p  className={` ${sidebar?"":"hidden"}`}>Liked Videos</p>
+            <p className={` ${sidebar ? "" : "hidden"}`}>Liked Videos</p>
           </div>
-          <hr className="w-10"/>
+          <hr className="w-10" />
         </div>
         {/* Subscription */}
-        <div>
+        <div className={` ${sidebar ? "" : "hidden"}`}>
           <h3 className="font-bold">Subscriptions</h3>
           <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
-            <img src={Image1} alt="" className="rounded-full h-5 w-5"/>
-            <p className={` ${sidebar?"":"hidden"}`}>Kana Television</p>
+            <img src={Image1} alt="" className="rounded-full h-5 w-5" />
+            <p>Kana Television</p>
           </div>
           <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
-            <img src={Image2} alt="" className="rounded-full h-5 w-5"/>
-            <p className={` ${sidebar?"":"hidden"}`}>MrBeast</p>
+            <img src={Image2} alt="" className="rounded-full h-5 w-5" />
+            <p className={` ${sidebar ? "" : "hidden"}`}>MrBeast</p>
           </div>
           <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
-            <img src={Image2} alt="" className="rounded-full h-5 w-5"/>
-            <p className={` ${sidebar?"":"hidden"}`}>Abel Birhanu</p>
+            <img src={Image2} alt="" className="rounded-full h-5 w-5" />
+            <p className={` ${sidebar ? "" : "hidden"}`}>Abel Birhanu</p>
           </div>
           <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
-          <RiArrowDropDownLine /> 
-          <h3 className={` ${sidebar?"":"text-sm"}`}>Show more</h3>
+            <RiArrowDropDownLine />
+            <h3 className={` ${sidebar ? "" : "text-sm"}`}>Show more</h3>
           </div>
-          <hr className="w-10"/>
-
+          <hr className="w-10" />
         </div>
         {/* Explore */}
-        <div>
+        <div className={` ${sidebar ? "" : "hidden"}`}>
           <h3 className="font-bold">Explore</h3>
 
           <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
             <FaMusic className="h-5 w-5 mr-3" />
-            <p className={` ${sidebar?"":"hidden"}`}>Music</p>
+            <p className={` ${sidebar ? "" : "hidden"}`}>Music</p>
           </div>
           <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
             <MdOnlinePrediction className="h-5 w-5 mr-3" />
-            <p className={` ${sidebar?"":"hidden"}`}>Live</p>
+            <p className={` ${sidebar ? "" : "hidden"}`}>Live</p>
           </div>
           <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
             <SiYoutubegaming className="h-5 w-5 mr-3" />
-            <p className={` ${sidebar?"":"hidden"}`}>Gaming</p>
+            <p className={` ${sidebar ? "" : "hidden"}`}>Gaming</p>
           </div>
           <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
             <FaRegNewspaper className="h-5 w-5 mr-3" />
-            <p className={` ${sidebar?"":"hidden"}`}>News</p>
+            <p className={` ${sidebar ? "" : "hidden"}`}>News</p>
           </div>
           <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
             <GiTrophyCup className="h-5 w-5 mr-3" />
-            <p className={` ${sidebar?"":"hidden"}`}>Sports</p>
+            <p className={` ${sidebar ? "" : "hidden"}`}>Sports</p>
           </div>
           <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
             <BsLightbulb className="h-5 w-5 mr-3" />
-            <p className={` ${sidebar?"":"hidden"}`}>Learning</p>
+            <p className={` ${sidebar ? "" : "hidden"}`}>Learning</p>
           </div>
           <div className="flex items-center mb-2 p-2 rounded hover:bg-gray-200">
             <FaBasketShopping className="h-5 w-5 mr-3" />
-            <p className={` ${sidebar?"":"hidden"}`}>Fashion & Beauty</p>
+            <p className={` ${sidebar ? "" : "hidden"}`}>Fashion & Beauty</p>
           </div>
         </div>
       </div>
