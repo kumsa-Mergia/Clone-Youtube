11c71cf3481329277b2fa72fc09d895efb91824f
kumsa Mergia <kumsamergia@gmail.com>
2025-03-04 13:39:24 +0300

    FeedvSection

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,54 +1,20 @@
 import React from "react";
 import myimage from "../../assets/img/1.jpg"
+import { Link } from "react-router-dom";
 
 const Feed = () => {
   return (
   <>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-7">
     {/* Card 1 */}
-    <div>
+    <Link to={`video/20/25`}>
     <img src={myimage} alt="" className="h-[240px] w-full object-cover"/>
-    <h2 className="text-xl font-semibold">Best Channel to learn coding that help you to a front end</h2>
-    <h3 className="text-gray-500">Greatstack</h3>
+    <h2 className="text-xl font-semibold">Introduction to Information Security | Essential Concepts and
+    Practices</h2>
+    <h3 className="text-gray-500">Channel Name</h3>
     <p className="text-gray-500">17K views &bull; 4 days ago</p>
-    </div>
-    {/* Card 2 */}
-    <div>
-    <img src={myimage} alt="" className="h-[240px] w-full object-cover"/>
-    <h2 className="text-xl font-semibold">Best Channel to learn coding that help you to a front end</h2>
-    <h3 className="text-gray-500">Greatstack</h3>
-    <p className="text-gray-500">17K views &bull; 4 days ago</p>
-    </div>
-        {/* Card 1 */}
-        <div>
-    <img src={myimage} alt="" className="h-[240px] w-full object-cover"/>
-    <h2 className="text-xl font-semibold">Best Channel to learn coding that help you to a front end</h2>
-    <h3 className="text-gray-500">Greatstack</h3>
-    <p className="text-gray-500">17K views &bull; 4 days ago</p>
-    </div>
-    {/* Card 2 */}
-    <div>
-    <img src={myimage} alt="" className="h-[240px] w-full object-cover"/>
-    <h2 className="text-xl font-semibold">Best Channel to learn coding that help you to a front end</h2>
-    <h3 className="text-gray-500">Greatstack</h3>
-    <p className="text-gray-500">17K views &bull; 4 days ago</p>
-    </div>
-    {/* Card 2 */}
-
-    <div>
-    <img src={myimage} alt="" className="h-[240px] w-full object-cover"/>
-    <h2 className="text-xl font-semibold">Best Channel to learn coding that help you to a front end</h2>
-    <h3 className="text-gray-500">Greatstack</h3>
-    <p className="text-gray-500">17K views &bull; 4 days ago</p>
-    </div>
-    {/* Card 2 */}
-
-    <div>
-    <img src={myimage} alt="" className="h-[240px] w-full object-cover"/>
-    <h2 className="text-xl font-semibold">Best Channel to learn coding that help you to a front end</h2>
-    <h3 className="text-gray-500">Greatstack</h3>
-    <p className="text-gray-500">17K views &bull; 4 days ago</p>
-    </div>
+    </Link>
+    
 
   </div>
   </>
